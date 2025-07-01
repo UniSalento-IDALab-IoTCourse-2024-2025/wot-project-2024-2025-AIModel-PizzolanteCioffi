0769ebf1e779a43d6b61a60e3ae83c1cc2e6203a
desss2 <desiree.pizzolante@studenti.unisalento.it>
2025-07-01 16:50:32 +0200

    svuoto repository

--- app/services/geocoding.py
+++
@@ -1,9 +0,0 @@
-from geopy.geocoders import Nominatim
-
-def geocode_address(address: str):
-    geolocator = Nominatim(user_agent="WatchApp")
-    location = geolocator.geocode(address, timeout=5)
-    if location:
-        return location.latitude, location.longitude
-    else:
-        return None, None
--- app/services/train.py
+++
@@ -1,3 +0,0 @@
-from app.services.inference import train_and_save_model
-
-train_and_save_model()
